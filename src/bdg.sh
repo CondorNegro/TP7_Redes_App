c43100a2eeedc9432c95cbe2d1934e95daa2b590
ser0090 <ser.0090@gmail.com>
2018-05-30 16:54:50 -0300

    pings andando

--- src/bdg.sh
+++ src/bdg.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 echo "#### BORRAR IPv4 default gateway ###"
-sudo docker exec src_h11_1 ip route del default
-sudo docker exec src_h13_1 ip route del default
-sudo docker exec src_r3_1 ip route del default
-sudo docker exec src_r5_1 ip route del default
-sudo docker exec src_r7_1 ip route del default
-sudo docker exec src_b1_1 ip route del default
+sudo docker exec -it src_h11_1 ip route del default
+sudo docker exec -it src_h13_1 ip route del default
+sudo docker exec -it src_r3_1 ip route del default
+sudo docker exec -it src_r5_1 ip route del default
+sudo docker exec -it src_r7_1 ip route del default
+sudo docker exec -it src_b1_1 ip route del default
 
 echo "### BORRAR IPv6 DEFAULT GATEWAY###" 
-sudo docker exec src_r3_1 ip -6 route del default
-sudo docker exec src_r5_1 ip -6 route del default
-sudo docker exec src_r7_1 ip -6 route del default
-sudo docker exec src_b1_1 ip -6 route del default
+sudo docker exec -it src_r3_1 ip -6 route del default
+sudo docker exec -it src_r5_1 ip -6 route del default
+sudo docker exec -it src_r7_1 ip -6 route del default
+sudo docker exec -it src_b1_1 ip -6 route del default
 
-sudo docker exec src_h11_1 ip -6 route del default
-sudo docker exec src_h13_1 ip -6 route del default
-sudo docker exec src_h11_1 ip -6 route add default via 2001:aaaa:aaaa:6::5
-sudo docker exec src_h13_1 ip -6 route add default via 2001:aaaa:aaaa:7::7
+sudo docker exec -it src_h11_1 ip -6 route del default
+sudo docker exec -it src_h13_1 ip -6 route del default
+sudo docker exec -it src_h11_1 ip -6 route add default via 2001:aaaa:aaaa:6::5
+sudo docker exec -it src_h13_1 ip -6 route add default via 2001:aaaa:aaaa:7::7
 
 echo "### AGREGANDO GATEWAY ESTATICO ###"
-sudo docker exec src_r3_1 ip -6 route add default via 2001:aaaa:aaaa:1::111
-sudo docker exec src_r5_1 ip -6 route add default via 2001:aaaa:aaaa:2::111
-sudo docker exec src_r7_1 ip -6 route add default via 2001:aaaa:aaaa:3::111
+sudo docker exec -it src_r3_1 ip -6 route add default via 2001:aaaa:aaaa:1::111
+sudo docker exec -it src_r5_1 ip -6 route add default via 2001:aaaa:aaaa:2::111
+sudo docker exec -it src_r7_1 ip -6 route add default via 2001:aaaa:aaaa:3::111
