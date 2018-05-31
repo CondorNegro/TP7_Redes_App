f52c4ab54fdef79fc0d563f11bdd5c66e0d9d4c3
ubuntu <lopez.gaston.1996@gmail.com>
2018-05-31 01:08:06 -0300

    Bind en docker

--- srcSA2/del_default_gateway.sh
+++ srcSA2/del_default_gateway.sh
@@ -4,28 +4,28 @@
 echo "Borro default gateways asignados por Docker"
 
 
-docker exec -it bgp_h12_1 ip -6 route del default
+docker exec -it srcsa2_h12_1 ip -6 route del default
 
-docker exec -it bgp_h14_1 ip -6 route del default
+docker exec -it srcsa2_h14_1 ip -6 route del default
 
-docker exec -it bgp_r4_1 ip -6 route del default
+docker exec -it srcsa2_r4_1 ip -6 route del default
 
 
-docker exec -it bgp_r6_1 ip -6 route del default
+docker exec -it srcsa2_r6_1 ip -6 route del default
 
-docker exec -it bgp_r8_1 ip -6 route del default
+docker exec -it srcsa2_r8_1 ip -6 route del default
 
-docker exec -it bgp_b2_1 ip -6 route del default
+docker exec -it srcsa2_b2_1 ip -6 route del default
 
 
 echo "Asigno nuevo default gw"
 
 
-docker exec -it bgp_h12_1 ip -6 route add default via 2001:b612::6
-docker exec -it bgp_h14_1 ip -6 route add default via 2001:b814::8
+docker exec -it srcsa2_h12_1 ip -6 route add default via 2001:bbbb:bbbb:612::6
+docker exec -it srcsa2_h14_1 ip -6 route add default via 2001:bbbb:bbbb:814::8
 
 echo "Asigno default gateways routers internos SA"
 
-docker exec -it bgp_r4_1 ip -6 route add default via 2001:bb24::2
-docker exec -it bgp_r6_1 ip -6 route add default via 2001:bb26::2
-docker exec -it bgp_r8_1 ip -6 route add default via 2001:bb28::2
+docker exec -it srcsa2_r4_1 ip -6 route add default via 2001:bbbb:bbbb:24::2
+docker exec -it srcsa2_r6_1 ip -6 route add default via 2001:bbbb:bbbb:26::2
+docker exec -it srcsa2_r8_1 ip -6 route add default via 2001:bbbb:bbbb:28::2
--- srcSA2/delete.sh
+++ srcSA2/delete.sh
@@ -3,12 +3,12 @@ echo "Borro lo creado"
 
 #Ejecutar con sudo.
 
-docker rm -f bgp_b2_1
-docker rm -f bgp_r4_1
+docker rm -f srcsa2_b2_1
+docker rm -f srcsa2_r4_1
 
-docker rm -f bgp_r6_1 bgp_r8_1
+docker rm -f srcsa2_r6_1 srcsa2_r8_1
 
-docker rm -f bgp_h12_1 bgp_h14_1 
+docker rm -f srcsa2_h12_1 srcsa2_h14_1 srcsa2_bind_1
 
 
 docker network prune
