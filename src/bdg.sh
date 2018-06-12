892bf3174beb6c4145452f31e9f37d3d7fe76f91
ser0090 <ser.0090@gmail.com>
2018-06-12 15:22:35 -0300

    area1

--- src/bdg.sh
+++ src/bdg.sh
@@ -6,13 +6,13 @@ sudo docker exec -it src_h13_1 ip route del default
 sudo docker exec -it src_r3_1 ip route del default
 sudo docker exec -it src_r5_1 ip route del default
 sudo docker exec -it src_r7_1 ip route del default
-sudo docker exec -it src_b1_1 ip route del default
+#sudo docker exec -it src_b1_1 ip route del default
 
 echo "### BORRAR IPv6 DEFAULT GATEWAY###" 
 sudo docker exec -it src_r3_1 ip -6 route del default
 sudo docker exec -it src_r5_1 ip -6 route del default
 sudo docker exec -it src_r7_1 ip -6 route del default
-sudo docker exec -it src_b1_1 ip -6 route del default
+#sudo docker exec -it src_b1_1 ip -6 route del default
 
 sudo docker exec -it src_h11_1 ip -6 route del default
 sudo docker exec -it src_h13_1 ip -6 route del default
