7b3e9754327d2f6e8418fe08957abab0e5a7b423
lopezgaston <gopezlaston@gmail.com>
2018-06-16 01:43:06 -0300

    Script

---
+++ src/script_config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "#### Configurar nameserver DNS ###"
+
+sudo docker exec -it src_h12_1 apt install dnsutils
+sudo docker exec -it src_h12_1 apt install nano
+sudo docker exec -it src_h12_1 sh -c "echo nameserver 2001:bbbb:bbbb:8::4 > /etc/resolv.conf"
+sudo docker exec -it src_h14_1 apt install dnsutils
+sudo docker exec -it src_h14_1 apt install nano
+sudo docker exec -it src_h14_1 sh -c "echo nameserver 2001:bbbb:bbbb:8::4 > /etc/resolv.conf"
+
+
+echo "### Configurar BIND ###" 
+sudo docker exec -it src_bind2_1 route del default
+sudo docker exec -it src_bind2_1 route -A inet6 add default gw 2001:bbbb:bbbb:8::3
+
+
+echo "### Configurar router de borde ###"
+sudo docker exec -it src_b2_1 ip -6 route add 2001:c:c:c::/64 via 2001:c:c:c::a
+sudo docker exec -it src_b2_1 ip -6 route del 2001:c:c:c::/64 dev eth0
+sudo docker exec -it src_b2_1 ip -6 route del default
+
+
